0b1d620069691b00de2def96ad517f8d8cf04d3e
asotito <84928376+asotito@users.noreply.github.com>
2024-09-02 22:53:25 -0500

    Modified functionality, process and added events and DOM's

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ let Name = '';
 const Products = ['Polo', 'Pantalon', 'Camisa', 'Zapatos', 'Corbata', 'Chaqueta', 'Gorra', 'Bufanda', 'Guantes', 'Calcetines'];
 const Select = document.getElementById('product-select');
 const reload = document.getElementById('reload');
-let totalItems = 0;
+let ProductList = [];
+let ProductsByBuyer = [];
+let firstSection = document.getElementById('first-section');
+let secondSection = document.getElementById('second-section');
+let thirdSection = document.getElementById('third-section');
+let fourthSection = document.getElementById('fourth-section');
+let fifthSection = document.getElementById('fifth-section');
 
 document.getElementById("button-plus").addEventListener("click", function() {
     addQuantity();
@@ -19,9 +25,13 @@ function addQuantity() {
     console.log(Quantity);
 }
 function nameF() {
-    Name = prompt("Introduzca su nombre:", "Andres Soto");
-    document.getElementById("name").textContent = Name;
-    console.log(Name);
+    Name = prompt("Introduzca su nombre:", '');
+    if (Name != ''){
+        document.getElementById("name").textContent = Name;
+        console.log(Name);
+        secondSection.classList.toggle('hidden');
+        thirdSection.classList.toggle('hidden');
+    }
 }
 
 function subtractQuantity() {
@@ -35,22 +45,60 @@ function subtractQuantity() {
 }
 
 function cleanWindow(){
-    if (confirm("Seguro que quiere continuar?")) {
+    if (confirm("Esta accion eliminara todas sus rutas, seguro que desea continuar?")) {
         localStorage.clear();
         window.location.reload();
     }
 }
 
-function addNewProduct() {
+let buttonAdd = document.getElementById('add')
+buttonAdd.onclick = () => {
     if (Quantity > 0 && Name != '' && Select.value != '') {
         if (confirm("Seguro que quiere continuar?")) {
-            localStorage.setItem(Select.value, Quantity)
+            for (i = 0; i <= localStorage.length; i++) {
+                const key = localStorage.key(i);
+                const values = localStorage.getItem(key);
+                console.log(i, key, values, Select.value);
+                if (Name === key) {
+                    ProductsByBuyer.push( values );
+                    ProductsByBuyer.push(Quantity + 'x ' + Select.value); 
+                }
+            }
+            let keys = Object.keys(localStorage)
+            if(!keys.includes(Name)){
+                localStorage.setItem(Name, Quantity + 'x ' + Select.value);
+            }else{
+                localStorage.setItem(Name, ProductsByBuyer)
+            }
+            
             window.location.reload();
         }
     }  else {
         alert("Por favor, complete todos los campos");
     }
 }
+
+let buttonStart = document.getElementById('start');
+
+
+buttonStart.addEventListener("click", function(){
+    firstSection.classList.toggle('hidden');
+    secondSection.classList.toggle('hidden');
+});
+
+let buttonNext = document.getElementById('next');
+
+buttonNext.addEventListener("click", function(){
+    thirdSection.classList.toggle('hidden');
+    fourthSection.classList.toggle('hidden');
+});
+
+let buttonSecondNext = document.getElementById('second-next');
+
+buttonSecondNext.addEventListener("click", function(){
+    fourthSection.classList.toggle('hidden');
+    fifthSection.classList.toggle('hidden');
+});
 
 Products.forEach(Product => {
     const option = document.createElement("option");
@@ -67,10 +115,9 @@ for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
     console.log(`${key}: ${value}`);
-    for (let j  = 0; j < value; j++) {
-        totalItems++;
-    }
-    document.getElementById("total-quantity").textContent = totalItems;
+    ProductList.push(`${key}: ${value};`);
+    document.getElementById("total-routes").textContent = localStorage.length;
+    document.getElementById("product-list").innerHTML = ProductList.join('<br>');
 }
 
-console.log("Total de productos:", totalItems);
+console.log("Total de rutas:", localStorage.length);
