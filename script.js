9fba569983dd47bab47511dc0f29e74ed3907526
Andres Soto <84928376+asotito@users.noreply.github.com>
2024-09-03 18:18:17 -0500

    fix: removed alert and confirm messages, also removed onclick from html. And added confirm view, and next buttons

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let Quantity = 0;
-let Name = '';
 const Products = ['Polo', 'Pantalon', 'Camisa', 'Zapatos', 'Corbata', 'Chaqueta', 'Gorra', 'Bufanda', 'Guantes', 'Calcetines'];
 const Select = document.getElementById('product-select');
 const reload = document.getElementById('reload');
@@ -10,95 +9,97 @@ let secondSection = document.getElementById('second-section');
 let thirdSection = document.getElementById('third-section');
 let fourthSection = document.getElementById('fourth-section');
 let fifthSection = document.getElementById('fifth-section');
+let confirmSection = document.getElementById('confirm-section');
+let Direccion = '';
 
 document.getElementById("button-plus").addEventListener("click", function() {
-    addQuantity();
-});
-
-document.getElementById("button-minus").addEventListener("click", function() {
-    subtractQuantity();
-});
-
-function addQuantity() {
     Quantity++;
     document.getElementById("quantity-display").textContent = Quantity;
     console.log(Quantity);
-}
-function nameF() {
-    Name = prompt("Introduzca su nombre:", '');
-    if (Name != ''){
-        document.getElementById("name").textContent = Name;
-        console.log(Name);
-        secondSection.classList.toggle('hidden');
-        thirdSection.classList.toggle('hidden');
-    }
-}
+});
 
-function subtractQuantity() {
+document.getElementById("button-minus").addEventListener("click", function() {
     if (Quantity > 0) {
         Quantity--;
         document.getElementById("quantity-display").textContent = Quantity;
         console.log(Quantity);
     } else if (Quantity <= 0) {
-        alert("Cantidad no puede ser menor a 0");
+        let alertMessageM = document.getElementById('alert-message-minus');
+        alertMessageM.classList.toggle('hidden')
     }
-}
+});
 
-function cleanWindow(){
-    if (confirm("Esta accion eliminara todas sus rutas, seguro que desea continuar?")) {
-        localStorage.clear();
-        window.location.reload();
-    }
-}
-
-let buttonAdd = document.getElementById('add')
-buttonAdd.onclick = () => {
-    if (Quantity > 0 && Name != '' && Select.value != '') {
-        if (confirm("Seguro que quiere continuar?")) {
-            for (i = 0; i <= localStorage.length; i++) {
-                const key = localStorage.key(i);
-                const values = localStorage.getItem(key);
-                console.log(i, key, values, Select.value);
-                if (Name === key) {
-                    ProductsByBuyer.push( values );
-                    ProductsByBuyer.push(Quantity + 'x ' + Select.value); 
-                }
-            }
-            let keys = Object.keys(localStorage)
-            if(!keys.includes(Name)){
-                localStorage.setItem(Name, Quantity + 'x ' + Select.value);
-            }else{
-                localStorage.setItem(Name, ProductsByBuyer)
-            }
-            
-            window.location.reload();
-        }
-    }  else {
-        alert("Por favor, complete todos los campos");
-    }
+let buttonErase = document.getElementById('erase-all');
+buttonErase.onclick = () => {
+  firstSection.classList.toggle('hidden');
+  confirmSection.classList.toggle('hidden');
 }
 
 let buttonStart = document.getElementById('start');
-
-
 buttonStart.addEventListener("click", function(){
     firstSection.classList.toggle('hidden');
     secondSection.classList.toggle('hidden');
 });
 
-let buttonNext = document.getElementById('next');
+let buttonAdd = document.getElementById('add')
+buttonAdd.onclick = () => {
+    for (i = 0; i <= localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const values = localStorage.getItem(key);
+      console.log(i, key, values, Select.value);
+      if (Direccion === key) {
+        ProductsByBuyer.push( values );
+        ProductsByBuyer.push(Quantity + 'x ' + Select.value); 
+      }
+    }
+    let keys = Object.keys(localStorage)
+    if(!keys.includes(Direccion)){
+      localStorage.setItem(Direccion, Quantity + 'x ' + Select.value);
+    }else{
+      localStorage.setItem(Direccion, ProductsByBuyer)
+    }
+            
+    window.location.reload();
+}
 
+let buttonEraseAll = document.getElementById('erase');
+buttonEraseAll.addEventListener("click", function(){
+  localStorage.clear();
+  firstSection.classList.toggle('hidden');
+  confirmSection.classList.toggle('hidden');
+  window.reload();
+})
+
+let buttonCancel = document.getElementById('cancel');
+buttonCancel.addEventListener("click", function(){
+  firstSection.classList.toggle('hidden');
+  confirmSection.classList.toggle('hidden');
+})
+
+let buttonNext = document.getElementById('next');
 buttonNext.addEventListener("click", function(){
+    Direccion = document.getElementById('direccion').value;
+    if (Direccion === ""){
+      let alertMessageI = document.getElementById('alert-message-input');
+      alertMessageI.classList.toggle('hidden');
+    } else {
+      secondSection.classList.toggle('hidden');
+      thirdSection.classList.toggle('hidden');
+    }
+});
+
+let buttonSecondNext = document.getElementById('second-next');
+buttonSecondNext.addEventListener("click", function(){
     thirdSection.classList.toggle('hidden');
     fourthSection.classList.toggle('hidden');
 });
 
-let buttonSecondNext = document.getElementById('second-next');
-
-buttonSecondNext.addEventListener("click", function(){
+let buttonThirdNext = document.getElementById('third-next');
+buttonThirdNext.addEventListener("click", function(){
     fourthSection.classList.toggle('hidden');
     fifthSection.classList.toggle('hidden');
 });
+
 
 Products.forEach(Product => {
     const option = document.createElement("option");
